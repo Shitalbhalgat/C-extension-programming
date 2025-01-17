4aa1b8f87b0de3553fdf1032e5060ae14ecabac3
Shitalbhalgat <shitalbhalgat@gmail.com>
2025-01-17 18:14:37 +0530

    Opr

---
+++ operators.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main()
+{// logical operator
+   /* int a = 5, b = 5, c = 10, result;
+
+    result = (a == b) && (c > b);
+    printf("(a == b) && (c > b) is %d \n", result);
+
+    result = (a == b) && (c < b);
+    printf("(a == b) && (c < b) is %d \n", result);
+
+    result = (a == b) || (c < b);
+    printf("(a == b) || (c < b) is %d \n", result);
+
+    result = (a != b) || (c < b);
+    printf("(a != b) || (c < b) is %d \n", result);
+
+    result = !(a != b);
+    printf("!(a != b) is %d \n", result);
+
+    result = !(a == b);
+    printf("!(a == b) is %d \n", result);*/
+    //Bitwise operator
+    /*int a=10,b=2;
+printf("(a & b) is %d \n", a&b);
+printf("(a | b) is %d \n", a|b);
+printf("(a ^ b) is %d \n", a^b);
+printf("(a << b) is %d \n", a<<b);
+printf("(a >> b) is %d \n", a>>b);*/
+//ternary operator or conditional operators
+int a=10,b;
+b=(!a)?printf("True"):printf("False");
+
+    //sizeof operator
+   /* int a;
+    float b;
+    double c;
+    char d;
+    printf("Size of int=%d bytes\n",sizeof(a));
+    printf("Size of float=%d bytes\n",sizeof(b));
+    printf("Size of double=%d bytes\n",sizeof(c));
+    printf("Size of char=%d byte\n",sizeof(d));*/
+
+    return 0;
+}
