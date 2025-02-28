fa35d044ee8a19d52138c14f43a5d00ceb0daa30
Shitalbhalgat <shitalbhalgat@gmail.com>
2025-02-28 10:28:45 +0530

    pure virtual function

--- pure_virtual_function.cpp
+++ pure_virtual_function.cpp
@@ -1,37 +1,72 @@
-// C++ Program to illustrate the abstract class and virtual
-// functions
 #include <iostream>
 using namespace std;
 
-class Base {
-    // private member variable
-    int x;
-
+class Base
+ {
 public:
     // pure virtual function
     virtual void fun() = 0;
-
-    // getter function to access x
-    int getX() { return x; }
+   
 };
 
-// This class inherits from Base and implements fun()
-class Derived : public Base {
-    // private member variable
-    int y;
-
+class Derived : public Base 
+{
 public:
     // implementation of the pure virtual function
-    void fun() { cout << "fun() called"; }
+    void fun() 
+    { 
+        cout << "fun() called";
+     }
 };
 
-int main(void)
+int main()
 {
-    // creating an object of Derived class
+    
     Derived d;
-
-    // calling the fun() function of Derived class
     d.fun();
-
     return 0;
 }
+// // Abstract class  
+// class Shape 
+// {
+//     public: 
+//     virtual float Area() = 0; // Area is a pure virtual function   
+// };
+// class Square: public Shape 
+// {
+//     float a;
+//     public:
+//         Square(float l) 
+//         {
+//             a = l;
+//         }
+//     float Area()
+//      {
+//         return a * a; // returns area of square
+//     }
+// };
+// class Circle: public Shape 
+// {
+//     float r;
+//     public:
+// 
+//         Circle(float x) 
+//         {
+//             r = x;
+//         }
+//     float Area() 
+//     {
+//         return 3.14 * r * r;
+//     }
+// };
+// int main()
+// {
+//     Shape * shape;
+//     Square s(2);
+//     Circle c(4);
+//     shape = &s;
+//     cout<<"The area of square is: "<<shape -> Area()<<endl;
+//     shape = &c;
+//     cout<< "The area of  circle is: "<<shape -> Area()<<endl;
+//     return 0;
+// }
